20704f99733bfbe5f241b3793fbe7d1850372b72
GAMBHIR <prabhjyot.gambhir@keysight.com>
2018-05-19 14:36:09 +0530

    procfile

--- server/app.js
+++ server/app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
+const distDir = __dirname + "./dist/";
+const path = require('path');
+var appDir = path.dirname(require.main.filename);
 
-
-app.get('/', (req,res) => {
+app.get('/api', (req,res) => {
     res.send('Working');
 });
 
+app.use(express.static(path.join(__dirname, '/../dist')));
+
+
 // Server running at port 3000
-app.listen(process.env.PORT, (req,res) => {
+app.listen(2000, (req,res) => {
     console.log('Server running at port: ', process.env.PORT);
 });
 
-var distDir = __dirname + "/dist/";
-app.use(express.static(distDir));   
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join('../'  +__dirname, 'dist/index.html'));
+//   });
+
+app.get('/', function(req, res) { res.sendFile(path.join(__dirname + '/../dist/index.html')); })
+
